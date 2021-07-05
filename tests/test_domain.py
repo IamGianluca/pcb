a12410170e884db581bc3e53384c85b7af6af1e2
Gianluca Rossi <gr.gianlucarossi@gmail.com>
2021-07-05 13:44:03 -0400

    ENH: add assign_bus_rule() function (#1)

--- tests/test_domain.py
+++ tests/test_domain.py
@@ -1,5 +1,5 @@
 import pcb.domain.model as model
-from pcb.domain.model import MIN_WIDTH
+from pcb.domain.model import NetRule
 
 
 def test_bus_rule_assignement():
@@ -10,9 +10,30 @@ def test_bus_rule_assignement():
     a.add_net(a0)
     a.add_net(a1)
 
-    min_width_4 = model.NetRule.create_rule(MIN_WIDTH, 4)
+    rule_type = "MIN_WIDTH"
+    min_width_4 = model.NetRule.create_rule(rule_type, 4)
     a.assign_rule(min_width_4)
 
     assert min_width_4 in a0.assigned_rules()
-    assert a0.get_rule(MIN_WIDTH) == min_width_4
-    assert a1.get_rule(MIN_WIDTH) == min_width_4
+    assert a0.get_rule(rule_type) == min_width_4
+    assert a1.get_rule(rule_type) == min_width_4
+
+
+def test_soemthing():
+    # given
+    bus_name, rule_type, parameter = "a", "a_rule_type", 16
+    bus = model.Bus(bus_name)
+
+    a0 = model.Net("a0")
+    a1 = model.Net("a1")
+    bus.add_net(a0)
+    bus.add_net(a1)
+
+    model.BusRepository.add(bus)
+
+    # when
+    model.assign_bus_rule(bus_name, rule_type, parameter)
+
+    # then
+    assert a0.get_rule(rule_type) == NetRule(rule_type, parameter)
+    assert a1.get_rule(rule_type) == NetRule(rule_type, parameter)
--- tests/test_pcb.py
+++ tests/test_pcb.py
@@ -2,4 +2,4 @@ from pcb import __version__
 
 
 def test_version():
-    assert __version__ == '0.1.0'
+    assert __version__ == "0.1.0"
